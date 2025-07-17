1f0016765e6cdfba8bfeef929fb04720b765e3fe
Raunak Gupta <kg73012@gmail.com>
2025-07-17 19:01:47 +0000

    day 4

---
+++ string.js
@@ -0,0 +1,11 @@
+const a="raunak"
+const b=60
+// console.log(`Hello my name is ${a} and my repo are ${b}`)
+
+const game=new String("raunakrg")
+console.log(game[5]);
+// charAt indexOf
+
+const f= "hello world this is my first javascript "
+console.log(f.replace("llo","yobvro"))
+console.log(f.split(" "))
