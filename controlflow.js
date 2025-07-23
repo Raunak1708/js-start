e4228290f4f59667d5f59fcde075fe3c41d06d4d
Raunak Gupta <kg73012@gmail.com>
2025-07-23 18:12:20 +0000

    switch

---
+++ controlflow.js
@@ -0,0 +1,54 @@
+// control flow
+// if
+// if(2=="2"){
+//     console.log("hello");
+// }
+// ==,>=,<=,<,>,!==,!=
+
+// const score=200;
+// if(score>100){
+//     const power="fly"
+//     console.log(`user power: ${power} `); 
+// }
+
+// const balance=500;
+// // if(balance<1000) console.log("yes");
+
+// if(balance<500){
+//     console.log("less than")
+// }
+// else if(balance<750){
+//     console.log("less than 750")
+// }
+// else{
+//     console.log("greater than 750")
+// }
+
+// const userloggedin=true;
+// const debitcard=true;
+// if(userloggedin&&debitcard){
+//     console.log("allowed")
+// }
+// else{
+//     console.log("not allowed")
+// }
+
+// switch
+const month=3;
+switch (month) {
+    case 1:
+        console.log("january")
+        break;
+    case 2:
+        console.log("february")
+        break;
+    case 3:
+        console.log("march")
+        break;
+    case 4:
+        console.log("april")
+        break;
+
+    default:
+        break;
+}
---
+++ truthy.js
@@ -0,0 +1,32 @@
+// truthy
+// const email="raunak@gmail.com"
+// if(email){
+//     console.log("yoo")
+// }
+// else{
+//     console.log("noo")
+// }
+
+// falsy values
+// false,0,"",-0,BigINt 0n,null ,NaN,undefined
+// truthy
+// true ,"0",'false'," ",[],{},function(){}
+// const obj={}
+// if(Object.keys(obj).length===0){
+//     console.log("empty object");
+    
+// }
+
+// Nullish coalescing operator(??): null undefined
+// let val1;
+// // val1=5??10;
+// // val1=null??100
+// val1=undefined??500
+// console.log(val1)
+
+// ternary operator
+// condition :true?false
+const price=100
+price>=80?console.log("true"):console.log("false")
+
+
